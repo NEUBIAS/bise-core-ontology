213ac76a22f14491c2ab860a524380e0286e524d
Alban Gaignard <aban.gaignard@univ-nantes.fr>
2017-04-06 16:07:29 +0200

    removed git submodule

---
+++ owl-documentation/genDoc.sh
@@ -0,0 +1,1 @@
+./specgen6.py --name='BISE Core Ontology' --indir=../../owl-ontology --ns=http://biii.info/data-model# --prefix=http://bise-eu.info/core-ontology# --ontofile=bise-core-ontology-v1.owl --templatedir=biii-template --outfile=bise-core-ontolgy.html --specurl=file:///Users/gaignard-a/Documents/Dev/neubias-data-model/owl-ontology/bise-core-ontology.html
